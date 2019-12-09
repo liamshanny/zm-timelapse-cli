941e79fec2ca200d20caf0d38afeea594eeff0de
Liam Shanny <lshanny@tripadvisor.com>
2019-12-09 15:30:35 -0500

    no message

--- tests/test_daytime_only.py
+++ tests/test_daytime_only.py
@@ -2,11 +2,11 @@
 import json
 import unittest
 from unittest.mock import patch
-import timelapse_generator
+from timelapse_generator import is_time_between
 from datetime import time, datetime
 
 class TestCLI(unittest.TestCase):
 
    def test_cli(self):
        date = datetime.now().time()
-       timelapse_generator.is_time_between(time(11, 00), time(21, 30), check_time=date)
+       is_time_between(time(11, 00), time(21, 30), check_time=date)
